fdf0aeb370f9364663845b9a01f9fb8abd2d172c
seongbeop <seongbeop0@gmail.com>
2024-02-13 16:42:15 +0900

    Spring JDBC Template

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -3,8 +3,8 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +12,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 class MemberServiceIntegrationTest {
-
 
     @Autowired MemberService memberService;
     @Autowired
@@ -43,8 +44,9 @@ class MemberServiceIntegrationTest {
         Long saveId = memberService.join(member);
 
         //then
-        Member findmember = memberService.findOne(saveId).get();
-        Assertions.assertThat(member.getName()).isEqualTo(findmember.getName());
+        Member findMember = memberService.findOne(saveId).get();
+
+        assertThat(member.getName()).isEqualTo(findMember.getName());
     }
 
     @Test
@@ -60,7 +62,7 @@ class MemberServiceIntegrationTest {
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
-        Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
 //        try {
 //            memberService.join(member2);

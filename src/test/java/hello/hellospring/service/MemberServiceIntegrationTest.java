f0cd99d249691ae111619f30d4b7ec2f38053526
seongbeop <seongbeop0@gmail.com>
2024-02-14 09:11:15 +0900

    Spring JPA

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,19 +1,13 @@
 package hello.hellospring.service;
-
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -24,7 +18,7 @@ class MemberServiceIntegrationTest {
     MemberRepository memberRepository;
 
 
-//    @BeforeEach
+    //    @BeforeEach
 //    public void beforeEach() {
 //        memberRepository = new MemoryMemberRepository();
 //        memberService = new MemberService(memberRepository);
@@ -35,45 +29,28 @@ class MemberServiceIntegrationTest {
 //    }
 //Transactional 때문에 필요 없음
     @Test
-    void 회원가입() {
-        //given
-        Member member = new Member();
-        member.setName("spring");
+    public void 회원가입() throws Exception {
 
-        //when
+        //Given
+        Member member = new Member();
+        member.setName("hello");
+        //When
         Long saveId = memberService.join(member);
 
-        //then
-        Member findMember = memberService.findOne(saveId).get();
-
-        assertThat(member.getName()).isEqualTo(findMember.getName());
+        //Then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
     }
 
     @Test
-    public void 중복_회원_예외() {
-        //given
+    public void 중복_회원_예외() throws Exception {
+//Given
         Member member1 = new Member();
         member1.setName("spring");
-
         Member member2 = new Member();
         member2.setName("spring");
-        //when
-
+//When
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
-
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-
-//        try {
-//            memberService.join(member2);
-//            fail();
-//        } catch (IllegalStateException e) {
-//            Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-//        }
-
-        //then
-
-    }
-
-
-}
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다. assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    } }
